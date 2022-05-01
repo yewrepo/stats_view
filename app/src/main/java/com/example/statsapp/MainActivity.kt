5938ae874da2e6985981090b998d172c65e66e1c
Dmitriy <adimas@protonmail.com>
2022-05-01 21:44:48 +0300

    Добавил transition

--- app/src/main/java/com/example/statsapp/MainActivity.kt
+++ app/src/main/java/com/example/statsapp/MainActivity.kt
@@ -2,18 +2,26 @@ package com.example.statsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.transition.AutoTransition
+import androidx.transition.Scene
+import androidx.transition.Transition
+import androidx.transition.TransitionManager
+import com.example.statsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val stats = findViewById<StatsView>(R.id.stats)
-        stats.data = listOf(
-            500f,
-            500f,
-            500f,
-            500f
-        )
+        binding.start.setOnClickListener {
+            val endScene = Scene.getSceneForLayout(binding.root1, R.layout.second_scene, this)
+            TransitionManager.go(endScene, AutoTransition().apply {
+                duration = 2000
+            })
+        }
     }
 }
